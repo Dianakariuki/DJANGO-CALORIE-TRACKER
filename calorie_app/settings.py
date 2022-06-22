d6a36e5eec0c2059af44c03c8334de66775f2856
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-22 11:30:29 +0300

    This commit adds the calorie project

--- calorie_app/settings.py
+++ calorie_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-awm#*h$op+)=%)+i=d)i_@q7k9ze0dmbut5mm^gi_fudis5ff-'
+SECRET_KEY = 'django-insecure-y8koil^^tx63)s39t1&2x8+pl6r@0u%gjjf@7o2%s7ml!za$#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
